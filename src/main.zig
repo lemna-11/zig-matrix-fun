da4bfdfda0b8389448bd981b2e2f1359a104255e
= <lemmensmano@gmail.com>
2023-05-20 08:52:20 +0000

    add randomiser and change type

--- src/main.zig
+++ src/main.zig
@@ -1,70 +1,19 @@
 const std = @import("std");
 const mat = @import("matrix.zig");
+const rand = std.rand.DefaultPrng;
 const allocator = std.heap.ArenaAllocator;
-const f16m = mat.Matrix(f16);
-const f32m = mat.Matrix(f32);
-const u32m = mat.Matrix(u32);
 const testing = std.testing;
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+test "init test" {
+    var a = allocator.init(std.heap.page_allocator);
+    var alloc = a.allocator();
+    _ = try mat.f32m.init(alloc, 5, 5);
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
-
-// uncomment to test whether or not it compiles
-// i dont know of a better way to go about this
-// if you do, please inform me
-// test "compile time type check" {
-//     try testing.expectError(std.on, mat.Matrix(bool));
-// }
-
-// not supported by current random impl
-test "initialise f16 matrix" {
-    var arena = allocator.init(std.heap.page_allocator);
-    var alloc = arena.allocator();
-    defer arena.deinit();
-    _ = try f16m.init(&alloc, 5, 5);
-}
-
-test "initialise f32 matrix" {
-    var arena = allocator.init(std.heap.page_allocator);
-    var alloc = arena.allocator();
-    defer arena.deinit();
-    _ = try f32m.init(&alloc, 5, 5);
-}
-
-test "initialise u32 matrix" {
-    var arena = allocator.init(std.heap.page_allocator);
-    var alloc = arena.allocator();
-    defer arena.deinit();
-    var rows: usize = 10;
-    var cols: usize = 10;
-    const m = try u32m.init(&alloc, rows, cols);
-    var i: usize = 0;
-    while (i < rows * cols) : (i += 1) {
-        try testing.expect(m.vals[i] == 0);
-    }
-}
-
-test "randomise f32 matrix" {
-    var arena = allocator.init(std.heap.page_allocator);
-    var alloc = arena.allocator();
-    defer arena.deinit();
-    var rows: usize = 10;
-    var cols: usize = 10;
-    var m = try f32m.init(&alloc, rows, cols);
-    try m.randomise();
-}
-
-test "randomise u32 matrix" {
-    var arena = allocator.init(std.heap.page_allocator);
-    var alloc = arena.allocator();
-    defer arena.deinit();
-    var rows: usize = 10;
-    var cols: usize = 10;
-    var m = try u32m.init(&alloc, rows, cols);
-    try m.randomise();
+test "randomise test" {
+    var a = allocator.init(std.heap.page_allocator);
+    var alloc = a.allocator();
+    var r = rand.init(42);
+    var matr = try mat.f32m.init(alloc, 5, 5);
+    try matr.randomise(r.random());
 }
--- src/matrix.zig
+++ src/matrix.zig
@@ -1,9 +1,28 @@
 const std = @import("std");
 const Alloc = std.heap.ArenaAllocator;
 
-const f16m = struct {
-    const self = @This();
-    var allocator: std.mem.Allocator = undefined;
+pub const f32m = struct {
+    values: [][]f32,
+    alloc: std.mem.Allocator,
+    const Self = @This();
 
-    pub fn init() !void {}
+    pub fn init(allocator: std.mem.Allocator, cols: usize, rows: usize) !Self {
+        var vals: [][]f32 = try allocator.alloc([]f32, cols);
+        var i: usize = 0;
+        while (i < rows) : (i += 1) {
+            vals[i] = try allocator.alloc(f32, rows);
+        }
+        return .{
+            .values = vals,
+            .alloc = allocator,
+        };
+    }
+
+    pub fn randomise(self: *Self, supplier: std.rand.Random) !void {
+        for (self.values, 0..) |col, i| {
+            for (col, 0..) |_, j| {
+                self.values[i][j] = supplier.float(f32);
+            }
+        }
+    }
 };
