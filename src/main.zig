76e3cc2114cdee528f981f87e5444169c40c665c
= <lemmensmano@gmail.com>
2023-05-21 12:51:43 +0000

    fix randomise and add debugging

--- src/main.zig
+++ src/main.zig
@@ -16,4 +16,18 @@ test "randomise test" {
     var r = rand.init(42);
     var matr = try mat.f32m.init(alloc, 5, 5);
     try matr.randomise(r.random());
+    matr.print();
+}
+
+test "dot product test" {
+    var a = allocator.init(std.heap.page_allocator);
+    var alloc = a.allocator();
+    var r = rand.init(42);
+    var mata = try mat.f32m.init(alloc, 10, 3);
+    try mata.randomise(r.random());
+    var matb = try mat.f32m.init(alloc, 3, 10);
+    try matb.randomise(r.random());
+    var matc = try mat.f32m.dot(alloc, mata, matb);
+    matc.print();
+    try testing.expect(matc.rows == 10 and matc.cols == 10);
 }
--- src/matrix.zig
+++ src/matrix.zig
@@ -3,18 +3,20 @@ const Alloc = std.heap.ArenaAllocator;
 
 pub const f32m = struct {
     values: [][]f32,
-    alloc: std.mem.Allocator,
+    rows: usize,
+    cols: usize,
     const Self = @This();
 
     pub fn init(allocator: std.mem.Allocator, cols: usize, rows: usize) !Self {
         var vals: [][]f32 = try allocator.alloc([]f32, cols);
         var i: usize = 0;
-        while (i < rows) : (i += 1) {
+        while (i < cols) : (i += 1) {
             vals[i] = try allocator.alloc(f32, rows);
         }
         return .{
             .values = vals,
-            .alloc = allocator,
+            .rows = rows,
+            .cols = cols,
         };
     }
 
@@ -23,6 +25,57 @@ pub const f32m = struct {
             for (col, 0..) |_, j| {
                 self.values[i][j] = supplier.float(f32);
             }
+        }
+    }
+
+    pub fn dot(allocator: std.mem.Allocator, a: Self, b: Self) !Self {
+        var returnMatrix = try Self.init(allocator, a.cols, b.rows);
+        var colR: usize = 0;
+        var rowR: usize = 0;
+
+        while (colR < returnMatrix.cols) : (colR += 1) {
+            while (rowR < returnMatrix.rows) : (rowR += 1) {
+                returnMatrix.values[colR][rowR] = 0;
+            }
+        }
+
+        var colA: usize = 0;
+        var rowA: usize = 0;
+        var colB: usize = 0;
+        var rowB: usize = 0;
+
+        while (colA < a.cols) : (colA += 1) {
+            while (rowA < a.rows) : (rowA += 1) {
+                while (colB < b.cols) : (colB += 1) {
+                    while (rowB < b.rows) : (rowB += 1) {
+                        returnMatrix.values[colA][rowB] += (a.values[colA][rowA] * b.values[colB][rowB]);
+                    }
+                    rowB = 0;
+                }
+                colB = 0;
+            }
+            rowA = 0;
+        }
+
+        return returnMatrix;
+    }
+
+    pub fn scalarMult(a: *Self, scalar: f32) !void {
+        for (a.values, 0..) |value, i| {
+            for (value, 0..) |_, j| {
+                a.values[i][j] *= scalar;
+            }
+        }
+    }
+
+    pub fn print(mat: Self) void {
+        std.debug.print("matrix: {}x{}\n", .{ mat.cols, mat.rows });
+        for (mat.values) |col| {
+            std.debug.print("\t", .{});
+            for (col) |value| {
+                std.debug.print("{3f}  ", .{value});
+            }
+            std.debug.print("\n", .{});
         }
     }
 };
